96f05acd934e927de15d624761d9f9579958222b
xeeraki <shafai86@gmail.com>
2017-07-28 18:25:40 +0200

    commnets added

--- led4.X/main.c
+++ led4.X/main.c
@@ -9,10 +9,15 @@
 #include <stdlib.h>
 #include <p32xxxx.h>
 int main(void){
-    int switches;
+    /*LED4 on the uC32 have pin number 13 
+     * (look at chipKit ref pin out table and PIC32 ref for functionality)
+     * which means TRISG and PORTG with bit number 6 
+     * should be used to control the LED4*/
+    int led4;
+    
     TRISGbits.TRISG6 = 0;
     while(1){
-        switches = (PORTGbits.RG6 = 1);
+        led4 = (PORTGbits.RG6 = 1);
     }
 }
 
