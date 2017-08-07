3194b9ef1de55ebc2fa0143291b559f2eaec0356
xeeraki <shafai86@gmail.com>
2017-08-07 18:59:40 +0200

    ned projescts added

--- led0-7.X/led0-7main.c
+++ led0-7.X/led0-7main.c
@@ -7,13 +7,48 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void delay(int ms){
+    int i;
+    for(i = ms; i > 0; i--);
+    
+    }
+
+    
 int main(){
-volatile int *trise = (volatile int*) 0xbf886100;
+    int i;
+    volatile int *trise = (volatile int*) 0xbf886100;
     //porte must be at an adress 4*4 below trise
-    volatile int *porte = (volatile int*) 0xbf88611f;
+    volatile int *porte = (volatile int*) 0xbf886110;
+    //volatile int *push_buttons = (volatile int*) 0xbf886100;
     volatile int *LED = (volatile int*) 0xbf886100;
     *trise &= ~0xff;
     while(1){
+        delay(1000000);
         *LED = (*porte = 0xff);
+        delay(10000);
+         *LED = (*porte = 0xfe);
+        delay(10000);
+         *LED = (*porte = 0xfd);
+        delay(10000);
+         *LED = (*porte = 0xfb);
+        delay(10000);
+         *LED = (*porte = 0xf7);
+        delay(10000);
+         *LED = (*porte = 0xef);
+        delay(10000);
+         *LED = (*porte = 0xdf);
+        delay(10000);
+         *LED = (*porte = 0xbf);
+        delay(10000);
+         *LED = (*porte = 0x7f);
+        delay(100000);
+        
+         
     }
-  }
+}
+
+
+
+  
--- led4.X/main.c
+++ led4.X/main.c
@@ -8,16 +8,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <p32xxxx.h>
+#include <xc.h>
+
 int main(void){
     /*LED4 on the uC32 have pin number 13 
      * (look at chipKit ref pin out table and PIC32 ref for functionality)
      * which means TRISG and PORTG with bit number 6 
      * should be used to control the LED4*/
-    int led4;
+    int led4, i;
     
     TRISGbits.TRISG6 = 0;
     while(1){
         led4 = (PORTGbits.RG6 = 1);
+        for(i = 0; i < 40000; i++);
     }
 }
 
